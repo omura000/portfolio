58191a71f76e40f9f98693ea88e811b8e46606d4
omura000 <x23026xx@aitech.ac.jp>
2024-07-24 16:51:28 +0900

    7/24 new

--- js01.js
+++ js01.js
@@ -13,4 +13,17 @@ document.addEventListener("DOMContentLoaded", () => {
             fast.style.display = "none";
         }, 1000); // 1秒後に完全に非表示
     }, 2000); // 2秒後にフェードアウト開始
+
+    const menuIcon = document.querySelector('#menu-icon');
+    const navbar = document.querySelector('#navbar');
+
+    menuIcon.addEventListener('click', () => {
+      if (navbar.style.display === 'block') {
+        navbar.style.display = 'none';
+        menuIcon.textContent = '☰'; // メニューが閉じているときはバツアイコンを表示
+      } else {
+        navbar.style.display = 'block';
+        menuIcon.textContent = '×'; // メニューが開いているときはハンバーガーアイコンを表示
+      }
+    });
 });
